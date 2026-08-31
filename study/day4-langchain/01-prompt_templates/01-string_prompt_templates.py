5050522905d05e98b25bfa90347ab4c496264bae
lm <120329698@qq.com>
2026-08-31 17:16:47 +0800

    feat: 添加LangChain prompt模板示例，包括字符串模板、聊天模板和消息占位符

--- study/day4-langchain/01-prompt_templates/01-string_prompt_templates.py
+++ study/day4-langchain/01-prompt_templates/01-string_prompt_templates.py
@@ -4,3 +4,4 @@ from langchain_core.prompts import PromptTemplate
 prompt_template = PromptTemplate.from_template("Tell me a joke about {topic}")
 result = prompt_template.invoke({"topic": "cats"})
 print(result)
+# messages=[SystemMessage(content='You are a helpful assistant', additional_kwargs={}, response_metadata={}), HumanMessage(content='Tell me a joke about cats', additional_kwargs={}, response_metadata={})]
